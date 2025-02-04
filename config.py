7b4dde4078ad1637a33f217bfac760c569981996
Coyote1987 <166886176+Coyote1987@users.noreply.github.com>
2025-02-04 11:26:12 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+### **2. config.py**  
+```python
+TOKEN = 7579871605:AAH4nIaHFVDn6bbiNuyV8dhrMQa3dza1dUA
+OWNER_ID =  Your user ID: 606953778
+Current chat ID: 606953778 # ID владельца бота
+START_BALANCE = 0.0000001  # Начальный баланс майнинга
